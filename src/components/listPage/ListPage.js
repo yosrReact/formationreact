60890e5fdc756bb0caa3244466e1660dbfaa4487
Yosr <yosr.naija@live.com>
2020-12-15 23:07:18 +0100

    fix service
    fix service

--- src/components/listPage/ListPage.js
+++ src/components/listPage/ListPage.js
@@ -16,7 +16,6 @@ function ListPage() {
   const [loading, setLoading] = useState(false)
   const [tasks, setTasks] = useState([])
 
-
   const [isVisible, setIsVisible] = useState(true)
   const toggleVisibility = () => {
     setIsVisible(!isVisible)
@@ -25,30 +24,39 @@ function ListPage() {
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true)
-      const result = await fetchTasks()
-      setTasks(result)
-      setLoading(false)
+      try {
+        const result = await fetchTasks()
+        setTasks(result)
+        setLoading(false)
+      } catch (e) {
+        console.log("error")
+        setLoading(false)
+      }
     }
     console.log("useEffect")
-
     fetchData()
   }, [])
 
-  
-
-  const addTask = async (title, duration) => {
+    const addTask = async (title, duration) => {
     try {
-      const newTask = await addTaskFromService(title, duration)
+      setLoading(true)
+      const newTask = await addTaskFromService({
+        title,
+        duration,
+      })
       setTasks([...tasks, newTask])
+      setLoading(false)
     } catch (e) {
       console.log("error")
     }
   }
   const deleteTask = async (id) => {
     try {
+      setLoading(true)
       await deleteTaskFromService(id)
       const newTasks = tasks.filter((task) => task.id !== id)
       setTasks(newTasks)
+      setLoading(false)
     } catch (e) {
       console.log("error")
     }
@@ -56,9 +64,14 @@ function ListPage() {
 
   const updateTask = async (id, title, duration) => {
     try {
-      const newTask = await updateTaskFromService(id, title, duration)
+      setLoading(true)
+      const newTask = await updateTaskFromService(id, {
+        title,
+        duration,
+      })
       const newTasks = tasks.map((task) => (task.id === id ? newTask : task))
       setTasks(newTasks)
+      setLoading(false)
     } catch (e) {
       console.log("error")
     }
