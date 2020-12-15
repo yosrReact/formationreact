df47212fc1a593bda99c3a3504d3ae13243c4f82
Yosr <yosr.naija@live.com>
2020-12-15 21:36:50 +0100

    fake service layer
    fake service layer

---
+++ src/services/tasks.service.js
@@ -0,0 +1,52 @@
+let tasks = [
+  {
+    id: "1",
+    title: "Learn html",
+    duration: 60,
+  },
+  {
+    id: "2",
+    title: "Learn react",
+    duration: 30,
+  },
+  {
+    id: "3",
+    title: "Learn node",
+    duration: 50,
+  },
+]
+
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+export const fetchTasks = async () => {
+  await delay(1000)
+  return tasks
+}
+export const fetchTasksByFilter = async (searchValue) => {
+  await delay(1000)
+  return tasks.filter((task) => task.title.includes(searchValue))
+}
+
+export const fetchTaskById = async (id) => {
+  const task = tasks.find((task) => task.id === id)
+  return task
+}
+
+export const addTask = async (title, duration) => {
+  const newTask = { id: tasks.length + 1, title, duration }
+
+  tasks=tasks.concat(newTask)
+  return newTask
+}
+
+export const deleteTask = async (id) => {
+  tasks = tasks.filter((task) => task.id !== id)
+}
+
+export const updateTask = async (id, title, duration) => {
+  const updatedTask = { id, title, duration }
+  tasks = tasks.map((task) => (task.id === id ? updatedTask : task))
+  return updatedTask
+}
