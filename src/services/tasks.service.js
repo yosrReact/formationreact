16555c3e5cbc46a881d522f77acc618e969ba88e
Yosr <yosr.naija@live.com>
2020-12-16 00:48:48 +0100

    fix services
    fix services

--- src/services/tasks.service.js
+++ src/services/tasks.service.js
@@ -30,23 +30,31 @@ export const fetchTasksByFilter = async (searchValue) => {
 }
 
 export const fetchTaskById = async (id) => {
+  await delay(1000)
   const task = tasks.find((task) => task.id === id)
   return task
 }
 
-export const addTask = async (title, duration) => {
-  const newTask = { id: tasks.length + 1, title, duration }
+export const addTask = async (task) => {
+  await delay(1000)
+  const newTask = {
+    id: tasks.length + 1,
+    title: task.title,
+    duration: task.duration,
+  }
 
-  tasks=tasks.concat(newTask)
+  tasks = tasks.concat(newTask)
   return newTask
 }
 
 export const deleteTask = async (id) => {
+  await delay(1000)
   tasks = tasks.filter((task) => task.id !== id)
 }
 
-export const updateTask = async (id, title, duration) => {
-  const updatedTask = { id, title, duration }
+export const updateTask = async (id, task) => {
+  await delay(1000)
+  const updatedTask = { id, title: task.title, duration: task.duration }
   tasks = tasks.map((task) => (task.id === id ? updatedTask : task))
   return updatedTask
 }
